6c9d319c86046875c648baa372d37af638ea6abc
dnirojas <you@example.com>
2025-06-28 12:42:41 -0300

    Tarea de variables Finalizado

---
+++ clase2/variables.py
@@ -0,0 +1,21 @@
+'''
+este es un comentario de varias lineas
+se puede escribir varias lineas de comentarios
+y se puede usar para explicar el codigo
+'''
+print ("hola mundo desde la unida ") 
+
+numero=10
+flotantante=10.5
+booleano=True
+
+# Imprimir las variables
+print (numero, flotantante, booleano)
+print("suma de enteros")
+a= 10
+b= 20
+print("la suma de dos numeros es:",a+b)
+print("la resta de dos numeros es:",a-b)
+print("la multiplicacion de dos numeros es:",a*b)
+b= 5 
+print("la division de dos numeros es:",a/b)

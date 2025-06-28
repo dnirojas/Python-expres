c7335c929f8ab6fc436bc19427f18b1bbd807cdf
dnirojas <you@example.com>
2025-06-28 12:59:35 -0300

    aritmeticos

---
+++ clase2/operadores_de_aritmeticos.py
@@ -0,0 +1,16 @@
+''''
+operadores de aritmeticos
+suma +
+resta -
+multiplicacion *
+division /
+modulo %
+potencia **'''
+a = 10
+b = 5
+print("la suma de dos numeros es:", a + b)
+print("la resta de dos numeros es:", a - b)
+print("la multiplicacion de dos numeros es:", a * b)
+print("la division de dos numeros es:", a / b)
+print("el modulo de dos numeros es:", a % b)
+print("la potencia de dos numeros es:", a ** b)

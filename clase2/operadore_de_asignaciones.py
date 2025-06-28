865988aad745ae62e0a86f8144d8825b661d0835
dnirojas <you@example.com>
2025-06-28 12:53:03 -0300

    Finalizado Operadores

---
+++ clase2/operadore_de_asignaciones.py
@@ -0,0 +1,26 @@
+'''
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+'''
+a=51
+b=50
+r=a == b
+print(r)
+r=100 != 10
+print(r)
+r=100 > 99
+print(r)
+r=100 < 10
+print(r)
+r=100 >= 1000
+print(r)
+r=100 <= 100
+print(r)
+
+edad = 10
+p= edad >= 18
+print("la persona es mayor de edad:", p)

71eb7a40a6345d491eb4d39f43fc45b3f968d17b
dnirojas <you@example.com>
2025-06-28 13:24:32 -0300

    entrada de datos

---
+++ clase2/entrada_de_datos.py
@@ -0,0 +1,7 @@
+'''
+Este módulo contiene funciones para la entrada de datos del usuario.
+'''
+nombre = input("Introduce su nombre : ")
+apellido = input("Introduce su apellido : ")
+print("Hola", nombre, apellido, "bienvenido al curso de Python.")
+print(f"Hola {nombre} {apellido}, bienvenido al curso de Python.")

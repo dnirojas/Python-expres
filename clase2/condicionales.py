3db90cfeee1a0e8f202b8a75027fb114492e7655
dnirojas <you@example.com>
2025-06-28 13:35:19 -0300

    condicionales finalizado

---
+++ clase2/condicionales.py
@@ -0,0 +1,10 @@
+'''
+condicionales
+'''
+dato=int(input('Ingrese un dato: '))
+if dato > 0:
+    print('El dato es positivo')
+elif dato < 0:
+    print('El dato es negativo')
+else:
+    print('El dato es cero')

795015e7a18e31dd1d9731f38c45e0fb1f1bc69a
dnirojas <rrojasdni@gmail.com>
2025-07-12 12:54:39 -0300

    funciones

---
+++ Clase_3/funciones.py
@@ -0,0 +1,18 @@
+'''
+ejemplo de funciones en python
+'''
+a=2
+b=5
+
+def suma(a, b):
+    return a + b
+
+resultado = suma(a, b)
+print("La suma es:", resultado)
+print("Resultado de la suma es:", suma(a,57))
+
+def resta(a, b):
+    return a - b
+
+resultado = resta(10, 4)
+print("La resta es:", resultado)

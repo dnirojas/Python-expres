a124876ad2c0c232da066a3037246c831a1bbf7c
dnirojas <rrojasdni@gmail.com>
2025-07-12 12:35:42 -0300

    while

---
+++ Clase_3/While.py
@@ -0,0 +1,8 @@
+'''
+Estructura de control While
+'''
+contador = 0  # Inicialización del contador
+while contador <= 5:  # Condición del bucle
+    print("Contador:", contador)  # Imprimir el valor del contador
+    contador += 1  # Incrementar el contador
+print("Bucle terminado")  # Mensaje al finalizar el bucle
